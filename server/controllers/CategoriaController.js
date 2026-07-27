fe30ac56ba60400f48b2012256a23ab97161f7ef
Allyson <allysonblackssj@gmail.com>
2026-07-27 17:28:55 -0300

    implementa CRUD completo da entidade Categoria

--- server/controllers/CategoriaController.js
+++ server/controllers/CategoriaController.js
@@ -1,3 +1,4 @@
+import CategoriaEntity from "../entities/CategoriaEntity.js";
 import CategoriaRepository from "../repositories/CategoriaRepository.js";
 
 export default class CategoriaController{
@@ -16,4 +17,96 @@ async listar(req,res){
             return res.status(500).json({msg: "Erro interno do servidor"});
     }
 }
+
+ async obter(req, res) {
+        try{
+            let {id} = req.params;
+
+            let categoria = await this.#repo.obter(id);
+            if(categoria == null)
+                return res.status(404).json({msg: "Categoria não encontrada!"});
+
+            return res.status(200).json(categoria);
+        }
+        catch(error) {
+            console.error(error);
+            return res.status(500).json({msg: "Erro interno do servidor"});
+        }
+    }
+
+      async gravar(req, res) {
+        try{
+            let {descricao} = req.body;
+
+            let entidade = new CategoriaEntity(0,descricao);
+            if(entidade.validar()) {
+                let result = await this.#repo.gravar(entidade);
+                
+                return res.status(201).json(entidade);
+            }
+            else {
+                return res.status(400).json({msg: "Parâmetros incorretos. Por favor confira as informações da categoria"});
+            }
+
+        }
+        catch(error) {
+            console.error(error);
+            return res.status(500).json({msg: "Erro interno do servidor"});
+        }
+    }
+
+    async deletar(req, res) {
+        try{
+            let {id} = req.params;
+            let categoria = await this.#repo.obter(id);
+            if(categoria == null)
+                return res.status(404).json({msg: "Categoria não encontrada!"})
+        
+            let result = await this.#repo.excluir(id);
+
+            if(result == true){
+                return res.status(200).json({msg: "Categoria excluída!"});
+            }
+            else {
+                throw new Error("Erro ao excluir a categoria do banco de dados");
+            }
+
+         }
+        catch(error) {
+            console.error(error);
+            return res.status(500).json({msg: "Erro interno do servidor"});
+        }
+    }
+
+    async atualizar(req, res) {
+        try{
+            let {id} = req.params;
+            let {descricao} = req.body;
+            let categoria = new CategoriaEntity(id,descricao);
+            if(categoria.validar() && id) {
+                let categoriaEncontrada = await this.#repo.obter(id);
+                if(categoriaEncontrada) {
+                    let result = await this.#repo.atualizar(categoria);
+                    if(result) {
+                        return res.status(200).json({msg: "Categoria atualizada com sucesso!"});
+                    }
+
+                    throw new Error("Erro ao atualizar categoria no banco de dados");
+                }
+                else {
+                    return res.status(404).json({msg: "Categoria não encontrada!"});
+                }
+                
+            }
+            else {
+                return res.status(400).json({msg: "Faltam informações para concluir a atualização!"});
+            }
+
+        }
+        catch(error) {
+            console.error(error);
+            return res.status(500).json({msg: "Erro interno do servidor"});
+        }
+    }
+
 }
--- server/routes/categoriaRoute.js
+++ server/routes/categoriaRoute.js
@@ -11,5 +11,33 @@ let ctrl = new CategoriaController();
 router.get("/", (req, res) => {
     ctrl.listar(req, res);
 });
+/*  #swagger.tags = ['Categorias']
+    #swagger.summary = 'Cadastra categorias'
+    #swagger.description = 'Cadastra uma nova categoria.'
+*/
+router.post("/", (req, res) => {
+    ctrl.gravar(req, res);
+});
+/*  #swagger.tags = ['Categorias']
+    #swagger.summary = 'Atualiza uma categoria'
+    #swagger.description = 'Atualiza a categoria desejada'
+*/
+router.put("/:id", (req, res) => {
+    ctrl.atualizar(req, res);
+});
+/*  #swagger.tags = ['Categorias']
+    #swagger.summary = 'Lista certas categorias'
+    #swagger.description = 'Retorna todas as categorias cadastradas de acordo com o parametro.'
+*/
+router.get("/:id", (req, res) => {
+    ctrl.obter(req, res);
+});
+/*  #swagger.tags = ['Categorias']
+    #swagger.summary = 'Deleta uma categoria'
+    #swagger.description = 'Deleta a categoria desejada'
+*/
+router.delete("/:id", (req, res) => {
+    ctrl.deletar(req, res);
+});
 
 export default router;
